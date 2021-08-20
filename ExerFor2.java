af89ea8d1305831081d4a335c0dd635b79cb4215
RogerioGNunes <rg.nunes2013@gmail.com>
2021-08-20 02:44:19 +0000

    envio exerfor2

---
+++ ExerFor2.java
@@ -0,0 +1,12 @@
+public class ExerFor2{
+    public static void main(String args[]){
+    
+    int soma = 0;
+        for(int valor=1; valor<=1000;valor++){
+            soma = soma + valor;
+
+        }
+        System.out.println(soma);
+    }
+
+}
